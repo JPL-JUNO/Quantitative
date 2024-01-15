c3a8957c706564a937b759fe6f948e3efe6fa4c8
Stephen CUI <junocuixuan@163.com>
2024-01-15 22:34:13 +0800

    BSM 估值的绘图

---
+++ DAP/code/ch02/BSM_value_plot.py
@@ -0,0 +1,15 @@
+"""
+@Title        : European Call Option Value Plot
+@Author(s)    : Stephen CUI
+@LastEditor(s): Stephen CUI
+@CreatedTime  : 2024-01-11 15:01:58
+@Description  : 
+"""
+
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'serif'
+sys.path.append('../ch05')
+from BSM_option_valuation import BSM_call_value  # NOQA: 402
