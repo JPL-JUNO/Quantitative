161640befd61ee98e749cc0d024934f4d1da5e99
Stephen CUI <junocuixuan@163.com>
2024-04-09 00:03:11 +0800

    移除黄金的价格

--- ML4AT-2nd/data/create_datasets.py
+++ ML4AT-2nd/data/create_datasets.py
@@ -109,7 +109,7 @@ securities = {
     "BAMLCC0A0CMTRIV": "US Corp Master TRI",
     "BAMLHYH0A0HYM2TRIV": "US High Yield TRI",
     "BAMLEMCBPITRIV": "Emerging Markets Corporate Plus TRI",
-    "GOLDAMGBD228NLBM": "Gold (London, USD)",
+    # "GOLDAMGBD228NLBM": "Gold (London, USD)",
     "DGS10": "10-Year Treasury CMR",
 }
 df = web.DataReader(name=list(securities.keys()), data_source="fred", start=2000)
